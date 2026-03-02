fe2abb34c6ee201a246ac99ecfa1168f50b94ed2
amr baker <codeBOKER@users.noreply.huggingface.co>
2026-03-02 00:15:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -75,11 +75,15 @@ async def telegram_webhook(request: Request):
             print("1----------TELEGRAM_TOKEN:", TELEGRAM_TOKEN)
             print("2---------TELEGRAM_URL:", TELEGRAM_URL)
             # Send back to Telegram
-            async with httpx.AsyncClient() as client:
-                await client.post(TELEGRAM_URL, json={
+            async with httpx.AsyncClient(verify=False) as client:
+            await client.post(
+                f"https://149.154.167.220/bot{TELEGRAM_TOKEN}/sendMessage",
+                headers={"Host": "api.telegram.org"},
+                json={
                     "chat_id": chat_id,
                     "text": ai_answer
-                })
+                }
+            )
                 
     return {"status": "ok"}
 
